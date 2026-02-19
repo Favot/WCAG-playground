a58f0519d37440e578d3648f6a7b4542b75eb4f9
Favot <70208301+Favot@users.noreply.github.com>
2026-02-19 23:37:06 +0100

    Refactor accessibility tests for pressable role requirements
    - Encapsulated existing tests for accessibility roles within a describe block for better organization and readability.
    - Maintained existing test cases to ensure compliance with accessibility standards for various role values.

--- engine/src/rules/pressable-role-required/index.test.tsx
+++ engine/src/rules/pressable-role-required/index.test.tsx
@@ -10,72 +10,74 @@ const run = (component: React.ReactElement<any>) => {
 
 // To inspect these components, run the example app under "Rules -> Button Role"
 
-it("throws if 'accessibilityRole' prop not defined", () => {
-  const Button = () => (
-    <TouchableOpacity>
-      <Image source={TestAssets.heart['32px']} />
-    </TouchableOpacity>
-  );
+describe('pressable role required', () => {
+  it("throws if 'accessibilityRole' prop not defined", () => {
+    const Button = () => (
+      <TouchableOpacity>
+        <Image source={TestAssets.heart['32px']} />
+      </TouchableOpacity>
+    );
 
-  expect(() => run(<Button />)).toThrow(rule.help.problem);
-});
+    expect(() => run(<Button />)).toThrow(rule.help.problem);
+  });
 
-it("throws if 'accessibilityRole' prop has a value other than 'button', 'link', 'imagebutton', 'radio', or 'tab'", () => {
-  const Button = () => (
-    <TouchableOpacity accessibilityRole={'text'}>
-      <Image source={TestAssets.heart['32px']} />
-    </TouchableOpacity>
-  );
+  it("throws if 'accessibilityRole' prop has a value other than 'button', 'link', 'imagebutton', 'radio', or 'tab'", () => {
+    const Button = () => (
+      <TouchableOpacity accessibilityRole={'text'}>
+        <Image source={TestAssets.heart['32px']} />
+      </TouchableOpacity>
+    );
 
-  expect(() => run(<Button />)).toThrow(rule.help.problem);
-});
+    expect(() => run(<Button />)).toThrow(rule.help.problem);
+  });
 
-it("doesn't throw if 'accessibilityRole' prop has the value 'button'", () => {
-  const Button = () => (
-    <TouchableOpacity accessibilityRole={'button'}>
-      <Image source={TestAssets.heart['32px']} />
-    </TouchableOpacity>
-  );
+  it("doesn't throw if 'accessibilityRole' prop has the value 'button'", () => {
+    const Button = () => (
+      <TouchableOpacity accessibilityRole={'button'}>
+        <Image source={TestAssets.heart['32px']} />
+      </TouchableOpacity>
+    );
 
-  expect(() => run(<Button />)).not.toThrow(rule.help.problem);
-});
+    expect(() => run(<Button />)).not.toThrow(rule.help.problem);
+  });
 
-it("doesn't throw if 'accessibilityRole' prop has the value 'link'", () => {
-  const Button = () => (
-    <TouchableOpacity accessibilityRole={'link'}>
-      <Text>This is a link.</Text>
-    </TouchableOpacity>
-  );
+  it("doesn't throw if 'accessibilityRole' prop has the value 'link'", () => {
+    const Button = () => (
+      <TouchableOpacity accessibilityRole={'link'}>
+        <Text>This is a link.</Text>
+      </TouchableOpacity>
+    );
 
-  expect(() => run(<Button />)).not.toThrow(rule.help.problem);
-});
+    expect(() => run(<Button />)).not.toThrow(rule.help.problem);
+  });
 
-it("doesn't throw if 'accessibilityRole' prop has the value 'imagebutton'", () => {
-  const Button = () => (
-    <TouchableOpacity accessibilityRole={'imagebutton'}>
-      <Image source={TestAssets.heart['32px']} />
-    </TouchableOpacity>
-  );
+  it("doesn't throw if 'accessibilityRole' prop has the value 'imagebutton'", () => {
+    const Button = () => (
+      <TouchableOpacity accessibilityRole={'imagebutton'}>
+        <Image source={TestAssets.heart['32px']} />
+      </TouchableOpacity>
+    );
 
-  expect(() => run(<Button />)).not.toThrow(rule.help.problem);
-});
+    expect(() => run(<Button />)).not.toThrow(rule.help.problem);
+  });
 
-it("doesn't throw if 'accessibilityRole' prop has the value 'radio'", () => {
-  const Button = () => (
-    <TouchableOpacity accessibilityRole={'radio'}>
-      <Image source={TestAssets.heart['32px']} />
-    </TouchableOpacity>
-  );
+  it("doesn't throw if 'accessibilityRole' prop has the value 'radio'", () => {
+    const Button = () => (
+      <TouchableOpacity accessibilityRole={'radio'}>
+        <Image source={TestAssets.heart['32px']} />
+      </TouchableOpacity>
+    );
 
-  expect(() => run(<Button />)).not.toThrow(rule.help.problem);
-});
+    expect(() => run(<Button />)).not.toThrow(rule.help.problem);
+  });
 
-it("doesn't throw if 'accessibilityRole' prop has the value 'tab'", () => {
-  const Button = () => (
-    <TouchableOpacity accessibilityRole={'tab'}>
-      <Image source={TestAssets.heart['32px']} />
-    </TouchableOpacity>
-  );
+  it("doesn't throw if 'accessibilityRole' prop has the value 'tab'", () => {
+    const Button = () => (
+      <TouchableOpacity accessibilityRole={'tab'}>
+        <Image source={TestAssets.heart['32px']} />
+      </TouchableOpacity>
+    );
 
-  expect(() => run(<Button />)).not.toThrow(rule.help.problem);
+    expect(() => run(<Button />)).not.toThrow(rule.help.problem);
+  });
 });
